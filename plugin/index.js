e1562a33d77feba13b0c5fcc80c003b9f3acfec3
yungu-2201999 <rguo1022@gmail.com>
2024-12-26 16:00:32 +0800

    log file

---
+++ plugin/index.js
@@ -0,0 +1,34 @@
+const pluginName = "ConfoundPlugin";
+const fs = require("fs");
+
+class ConfoundPlugin {
+  constructor({ filename }) {
+    this.filename = filename;
+  }
+  apply(compiler) {
+    const pluginName = ConfoundPlugin.name;
+
+    compiler.hooks.emit.tap(pluginName, (compilation) => {
+      /**
+       * 生成文件数量与文件大小的列表
+       */
+      const { assets } = compilation;
+      let content = `## 文件名     大小\r\n`;
+      for (const [filename, stat] of Object.entries(assets)) {
+        content += `${filename}    ${stat.size()}\r\n`;
+      }
+      assets[this.filename] = {
+        source() {
+          return content;
+        },
+        size() {
+          return content.length;
+        },
+      };
+
+      // end
+    });
+  }
+}
+
+module.exports = ConfoundPlugin;
